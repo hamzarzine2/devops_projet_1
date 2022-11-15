9a7090485bbdefebd8ef9d30353c941a9a62107f
hamzarzine2 <hamza.rzine@student.vinci.be>
2022-11-15 21:24:55 +0100

    "modification of test 1"

--- __test__/Game.test.js
+++ __test__/Game.test.js
@@ -3,15 +3,13 @@ const Game = require("../src/Game");
 describe("Tests for a score calculator in a bowling game with spare and strike", () => {
   const game = new Game();
   let turn;
-  let score1;
-  let score2;
+  let score;
 
   it("should update score ", () => {
-
     givenTurn(0);
     givenScore(3,2);
 
-    whenRoll(score1,score2);
+    whenRoll(score);
 
     expect(game.scoreTotal).toBe(5);
     expect(game.turnPlayed).toBe(1);
@@ -22,12 +20,11 @@ describe("Tests for a score calculator in a bowling game with spare and strike",
   function givenTurn(t){
     turn=t;
   }
-  function givenScore(s1,s2) {
-    score1 = s1;
-    score2 = s2;
+  function givenScore(s) {
+    score = s;
   }
-  function whenRoll(s1,s2) {
-    game.roll(score1,score2);
+  function whenRoll(s) {
+    game.roll(score);
   }
 
 });
--- src/Game.js
+++ src/Game.js
@@ -2,9 +2,8 @@ module.exports = class Game {
   turnPlayed=0;
   scoreTotal=0;
 
-  roll (score1,score2) {
-    this.scoreTotal+=score1;
-    this.scoreTotal+=score2;
+  roll (score) {
+    this.scoreTotal+=score;
     this.turnPlayed++;
   }
 
